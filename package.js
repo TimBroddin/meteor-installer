3a6a765ce033f2b77c6e1ba315b6080d7ea73012
Tim Broddin <tim.broddin@vrt.be>
2015-08-12 11:07:51 +0200

    add github url

--- package.js
+++ package.js
@@ -5,13 +5,9 @@ Npm.depends({
 
 Package.describe({
   name: 'timbroddin:installer',
-  version: '0.0.1',
-  // Brief, one-line summary of the package.
+  version: '0.0.2',
   summary: 'Makes installing boilerplate stuff easy peazy.',
-  // URL to the Git repository containing the source code for this package.
-  git: '',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
+  git: 'https://github.com/TimBroddin/meteor-installer.git',
   documentation: 'README.md'
 });
 
